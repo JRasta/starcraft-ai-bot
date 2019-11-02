53da9fb8b85862d832fba11d0b516b42e82905e0
Johnny Smith <jayts2k@googlemail.com>
2019-11-02 13:53:43 +0000

    Moved old examples to backup folders

--- sc2/building-an-army.py
+++ sc2/building-an-army.py
@@ -1,32 +1,32 @@
-import sc2
-from sc2 import run_game, maps, Race, Difficulty
-from sc2.player import Bot, Computer
-from sc2.ids.unit_typeid import *
+import pysc2
+from pysc2 import run_game, maps, Race, Difficulty
+from pysc2.player import Bot, Computer
+from pysc2.ids.unit_typeid import *
 
 
 class TerranBot(sc2.BotAI):
-    async def on_step(self, iteration):
-        await self.distribute_workers()  # in sc2/bot_ai.py
-        await self.build_workers()  # create workers in SC2
-        await self.build_sdepots()  # build supply depots
-        await self.build_vespene()  # build vespene buildings
-        await self.expand_base()  # expand to another location
-        await self.offensive_force_buildings()  # build army producing units
-        await self.build_offensive_force()  # build an army
+    def on_step(self, iteration):
+        self.distribute_workers()  # in sc2/bot_ai.py
+        self.build_workers()  # create workers in SC2
+        self.build_sdepots()  # build supply depots
+        self.build_vespene()  # build vespene buildings
+        self.expand_base()  # expand to another location
+        self.offensive_force_buildings()  # build army producing units
+        self.build_offensive_force()  # build an army
 
-    async def build_workers(self):
+    def build_workers(self):
         for cc in self.units(UnitTypeId.COMMANDCENTER).noqueue:
             if self.can_afford(UnitTypeId.SCV) and not self.units(UnitTypeId.SCV).amount == 16:
-                await self.do(cc.train(UnitTypeId.SCV))
+                self.do(cc.train(UnitTypeId.SCV))
 
-    async def build_sdepots(self):
+    def build_sdepots(self):
         if self.supply_left < 4 and not self.already_pending(UnitTypeId.SUPPLYDEPOT):
             commandc = self.units(UnitTypeId.COMMANDCENTER).ready
             if commandc.exists:
                 if self.can_afford(UnitTypeId.SUPPLYDEPOT):
-                    await self.build(UnitTypeId.SUPPLYDEPOT, near=commandc.random)
+                    self.build(UnitTypeId.SUPPLYDEPOT, near=commandc.random)
 
-    async def build_vespene(self):
+    def build_vespene(self):
         for cc in self.units(UnitTypeId.COMMANDCENTER).ready:
             gases = self.state.vespene_geyser.closer_than(9.0, cc)
             for gas in gases:
@@ -36,22 +36,22 @@ class TerranBot(sc2.BotAI):
                 if worker is None:
                     break
                 if not self.units(UnitTypeId.REFINERY).closer_than(1.0, gas).exists:
-                    await self.do(worker.build(UnitTypeId.REFINERY, gas))
+                    self.do(worker.build(UnitTypeId.REFINERY, gas))
 
-    async def expand_base(self):
+    def expand_base(self):
         if self.units(UnitTypeId.COMMANDCENTER).amount < 3 and self.can_afford(UnitTypeId.COMMANDCENTER):
-            await self.expand_now()
+            self.expand_now()
 
-    async def offensive_force_buildings(self):
+    def offensive_force_buildings(self):
         commandc = self.units(UnitTypeId.COMMANDCENTER).ready
         if not self.units(UnitTypeId.BARRACKS).ready.exists:
             if self.can_afford(UnitTypeId.BARRACKS) and not self.already_pending(UnitTypeId.BARRACKS):
-                await self.build(UnitTypeId.BARRACKS, near=commandc.random)
+                self.build(UnitTypeId.BARRACKS, near=commandc.random)
 
-    async def build_offensive_force(self):
+    def build_offensive_force(self):
         for barracks in self.units(UnitTypeId.BARRACKS).ready.noqueue:
             if self.can_afford(UnitTypeId.MARINE) and self.supply_left > 0:
-                await self.do(barracks.train(UnitTypeId.MARINE))
+                self.do(barracks.train(UnitTypeId.MARINE))
 
 
 run_game(maps.get("KingsCoveLE"), [
